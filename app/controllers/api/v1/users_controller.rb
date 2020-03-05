e83876c425ae32f48126bf2b36940d15a32527de
tuvsheezz <tuvshee0089@gmail.com>
2020-03-05 20:54:20 +0900

    init

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,13 +5,13 @@ class Api::V1::UsersController < ApplicationController
   before_action :check_login, only: %i[update destroy]
 
   def show
-    render json: @user
+    render json: UserSerializer.new(@user).serializable_hash
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -19,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: UserSerializer.new(@user).serializable_hash, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email
-  attributes :password_digest
-  attributes self.products
 end
